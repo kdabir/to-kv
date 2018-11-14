42508e5a6008f724b5ee0a4f3a3b9fd707c3e8cf
Kunal Dabir <kunal.dabir@gmail.com>
2018-11-14 23:48:35 +0530

    more specs

--- spec/index.spec.js
+++ spec/index.spec.js
@@ -38,6 +38,46 @@ describe("on conflict strategy", () => {
   });
 
 });
+describe("missing values", () => {
+
+  const arrOfObjects = [
+    {key: 'tag', value: undefined, extra: 'info about a'},
+    {key: 'tag', value: 'stuff', extra: 'info about b'},
+    {key: 'from', value: 'sender', extra: 'info about sender'},
+    {key: 'from',                 extra: 'info about sender'},
+    {key: 'to', value: 'receiver', extra: 'info about receiver'},
+    {key: 'to', value: null, extra: 'info about receiver'}
+  ];
+
+  it("should save last occurrence when using keepLast strategy", () => {
+    const convert = toKvConverter({onConflict: 'keepLast'});
+    const obj = convert(arrOfObjects);
+
+    expect(obj).toEqual({tag: 'stuff', from: 'sender', to: null})
+  });
+
+  it("should save first occurrence when using keepFirst strategy", () => {
+    const convert = toKvConverter({onConflict: 'keepFirst'});
+    const obj = convert(arrOfObjects);
+
+    expect(obj).toEqual({tag: 'cool', from: 'sender', to: 'receiver'})
+  });
+
+  it("should save all occurrence when using keepAll strategy", () => {
+    const convert = toKvConverter({onConflict: 'keepAll'});
+    const obj = convert(arrOfObjects);
+
+    expect(obj).toEqual({tag: ['cool', 'stuff'], from: ['sender'], to: ['receiver']})
+  });
+
+  it("should use keepLast as default strategy", () => {
+    const convert = toKvConverter();
+    const obj = convert(arrOfObjects);
+
+    expect(obj).toEqual({tag: 'stuff', from: 'sender', to: 'receiver'})
+  });
+
+});
 
 describe('option to change key and value names', () => {
 
